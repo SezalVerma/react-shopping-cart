0365a40ad4c95c12125bfcb6b1d25a3249c3e47a
sezalverma <sezal.verma@nagarro.com>
2021-11-12 10:30:57 +0530

    Moved state from Cart to App

--- src/App.js
+++ src/App.js
@@ -1,13 +1,83 @@
+import React from "react";
 import Cart from "./Cart";
 import Navbar from "./Navbar";
-function App() {
-  return (
-    <div className="App">
-      <Navbar />
-      <h1> Cart </h1>
-      <Cart />
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      products: [
+        {
+          price: 9999,
+          title: "Phone",
+          qty: 1,
+          img: "",
+          id: 1,
+        },
+        {
+          price: 999,
+          title: "Watch",
+          qty: 5,
+          img: "",
+          id: 2,
+        },
+        {
+          price: 99,
+          title: "Pen",
+          qty: 10,
+          img: "",
+          id: 3,
+        },
+      ],
+    };
+  }
+  handleIncreaseQty = (product) => {
+    const { products } = this.state;
+    // get index of desired product in arr of products
+    const index = products.indexOf(product);
+    products[index].qty += 1;
+    // set products in state to modified products
+    this.setState({
+      products: products,
+    });
+  };
+  handleDecreaseQty = (product) => {
+    const { products } = this.state;
+    const index = products.indexOf(product);
+    // if qty is 0, dont decrease
+    if (products[index].qty === 0) {
+      return;
+    }
+    products[index].qty -= 1;
+    // when key & value are same, pass once
+    this.setState({
+      products,
+    });
+  };
+  handleDeleteProduct = (id) => {
+    const { products } = this.state;
+
+    // add all products to items [] whose id doesnt match to given id,
+    const items = products.filter((item) => item.id !== id);
+
+    this.setState({
+      products: items,
+    });
+  };
+  render() {
+    const { products } = this.state;
+    return (
+      <div className="App">
+        <Navbar />
+        <h1> Cart </h1>
+        <Cart
+          products={products}
+          onIncreaseQty={this.handleIncreaseQty}
+          onDecreaseQty={this.handleDecreaseQty}
+          onDeleteProduct={this.handleDeleteProduct}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/Cart.js
+++ src/Cart.js
@@ -1,86 +1,24 @@
 import React, { Component } from "react";
 import CartItem from "./CartItem";
 
-class Cart extends Component {
-  constructor() {
-    super();
-    this.state = {
-      products: [
-        {
-          price: 9999,
-          title: "Phone",
-          qty: 1,
-          img: "",
-          id: 1,
-        },
-        {
-          price: 999,
-          title: "Watch",
-          qty: 5,
-          img: "",
-          id: 2,
-        },
-        {
-          price: 99,
-          title: "Pen",
-          qty: 10,
-          img: "",
-          id: 3,
-        },
-      ],
-    };
-  }
-  handleIncreaseQty = (product) => {
-    const { products } = this.state;
-    // get index of desired product in arr of products
-    const index = products.indexOf(product);
-    products[index].qty += 1;
-    // set products in state to modified products
-    this.setState({
-      products: products,
-    });
-  };
-  handleDecreaseQty = (product) => {
-    const { products } = this.state;
-    const index = products.indexOf(product);
-    // if qty is 0, dont decrease
-    if (products[index].qty === 0) {
-      return;
-    }
-    products[index].qty -= 1;
-    // when key & value are same, pass once
-    this.setState({
-      products,
-    });
-  };
-  handleDeleteProduct = (id) => {
-    const { products } = this.state;
-
-    // add all products to items [] whose id doesnt match to given id,
-    const items = products.filter((item) => item.id !== id);
-
-    this.setState({
-      products: items,
-    });
-  };
-  render() {
-    const { products } = this.state;
-    return (
-      <div className="cart">
-        {products.map((product) => {
-          return (
-            <CartItem
-              product={product}
-              key={product.id}
-              onIncreaseQty={this.handleIncreaseQty}
-              onDecreaseQty={this.handleDecreaseQty}
-              onDeleteProduct={this.handleDeleteProduct}
-            />
-          );
-        })}
-      </div>
-    );
-  }
-}
+// React pass props from parent to child in runtime
+const Cart = (props) => {
+  const { products } = props;
+  return (
+    <div className="cart">
+      {products.map((product) => {
+        return (
+          <CartItem
+            product={product}
+            key={product.id}
+            onIncreaseQty={props.onIncreaseQty}
+            onDecreaseQty={props.onDecreaseQty}
+            onDeleteProduct={props.onDeleteProduct}
+          />
+        );
+      })}
+    </div>
+  );
+};
 
 export default Cart;
